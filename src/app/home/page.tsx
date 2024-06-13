69bf3bd29a87edc5cfe009ff9739e0f6fb06c624
jetroot <lorean700@gmail.com>
2024-06-13 18:15:56 +0100

    feat: animate page before start fetching data with farmer motion

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,10 +3,11 @@
 import RocketCard from "@/components/RocketCard";
 import SkeletonCard from "@/components/SkeletonCard";
 import { useLaunches } from "@/services/queries";
-import toast from "react-hot-toast";
+import { GiFalconMoon } from "react-icons/gi";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  const { data, error, isLoading } = useLaunches();
+  const { data, isLoading } = useLaunches();
 
   if (isLoading)
     return (
@@ -17,17 +18,37 @@ const Home = () => {
       </div>
     );
 
-  if (error) {
-    toast.error(error.message);
-    return;
-  }
-
   return (
-    <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {data?.map((rocket: any) => (
-        <RocketCard key={rocket.flight_number} rocket={rocket} />
-      ))}
-    </div>
+    <>
+      {!data && (
+        <div
+          className={`absolute inset-0 flex h-full w-full items-center justify-center gap-2 overflow-hidden`}
+        >
+          <motion.div
+            animate={{
+              scale: 1,
+              rotate: [0, 360],
+              transition: {
+                duration: 3,
+              },
+              opacity: [0, 0.5, 1],
+            }}
+            className="flex cursor-default items-center justify-center gap-2"
+          >
+            <GiFalconMoon className="rounded-xl bg-gradient-to-tr from-green-300 to-indigo-400 text-9xl" />
+
+            <h1 className="bg-gradient-to-r from-green-400 to-indigo-400 bg-clip-text text-9xl font-bold uppercase text-transparent">
+              ACME
+            </h1>
+          </motion.div>
+        </div>
+      )}
+      <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {data?.map((rocket: any) => (
+          <RocketCard key={rocket.flight_number} rocket={rocket} />
+        ))}
+      </div>
+    </>
   );
 };
 
