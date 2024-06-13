a1d1730d7830d25da4c28237f1a1d8c0aeda3cba
jetroot <lorean700@gmail.com>
2024-06-13 18:23:28 +0100

    feat: add ability to specify baseUrl in fetcher

--- src/services/fetcher.ts
+++ src/services/fetcher.ts
@@ -1,16 +1,30 @@
 import axios from "axios";
+import toast from "react-hot-toast";
 
-export const axiosInstane = axios.create({
-  baseURL: "https://api.spacexdata.com/v3",
-});
+export const createAxiosInstance = (baseUrl = "http://localhost:3001/v1") => {
+  return axios.create({
+    baseURL: baseUrl,
+    withCredentials: true,
+  });
+};
 
-const fetcher = ({
-  url,
-  method,
-  data = null,
-}: {
-  url: string;
-  method: string;
-  data: any;
-}) => axiosInstane({ url, method, data }).then((res) => res.data);
+const fetcher = async (url: string, baseUrl?: string) => {
+  const axiosInstance = createAxiosInstance(baseUrl);
+
+  return axiosInstance
+    .get(url)
+    .then((res) => res.data)
+    .catch((err) => {
+      // throw err;
+
+      if (err?.response?.data?.message) {
+        toast.error(err.response.data.message);
+      } else {
+        toast.error(err.message);
+      }
+      // throw err;
+      return;
+    });
+};
+
 export default fetcher;
