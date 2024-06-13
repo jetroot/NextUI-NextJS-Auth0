4f21fbcacd8ae22ab7966da961085443fc51962a
jetroot <lorean700@gmail.com>
2024-06-13 18:25:57 +0100

    feat(auth): call useLaunches and useToken when authenticated

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,5 +1,18 @@
 import useSWR from "swr";
+import fetcher from "./fetcher";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export const useLaunches = () => {
-  return useSWR<any>({ url: "/launches" });
+  const { user, isLoading } = useUser();
+  const shouldFetch = !isLoading && user !== undefined;
+
+  return useSWR<any>(shouldFetch ? "/launches" : null);
+};
+
+export const useToken = (baseUrl?: string, uri = "/api/token") => {
+  const { user, isLoading } = useUser();
+  const shouldFetch = !isLoading && user !== undefined;
+
+  const url = baseUrl ? `${baseUrl}${uri}` : uri;
+  return useSWR<any>(shouldFetch ? url : null, () => fetcher(url, baseUrl));
 };
