52ee7db6b322f97ab00f25fae58e12a6d91ea38d
jetroot <lorean700@gmail.com>
2024-06-13 18:21:25 +0100

    feat: Add max error retry count to 3 in SWRConfig

--- src/providers/FetcherProvider.tsx
+++ src/providers/FetcherProvider.tsx
@@ -5,7 +5,9 @@ import { ReactNode } from "react";
 import { SWRConfig } from "swr";
 
 const FetcherProvider = ({ children }: { children: ReactNode }) => {
-  return <SWRConfig value={{ fetcher }}>{children}</SWRConfig>;
+  return (
+    <SWRConfig value={{ errorRetryCount: 3, fetcher }}>{children}</SWRConfig>
+  );
 };
 
 export default FetcherProvider;
